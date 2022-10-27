3f5b729e7f8318e59b182fe1fa2261cf99f5952b
JOÃO SANTOS <116624654+Gingseng@users.noreply.github.com>
2022-10-27 14:45:51 -0400

    Update teste2.py
    add new variables

--- teste2.py
+++ teste2.py
@@ -8,7 +8,7 @@ from gtts import gTTS
 from playsound import playsound  
   
 # It is a text value that we want to convert to audio  
-text_val = 'All the best for your exam.'  
+text_val = 'All the things we think is about thoughs.'  
   
 # Here are converting in English Language  
 language = 'en'  
@@ -21,7 +21,7 @@ obj = gTTS(text=text_val, lang=language, slow=False)
   
 #Here we are saving the transformed audio in a mp3 file named  
 # exam.mp3  
-obj.save("exam.mp3")  
+obj.save("teste.mp3")  
   
 # Play the exam.mp3 file  
-playsound("exam.mp3")  
+playsound("teste.mp3")  
